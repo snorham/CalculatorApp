39bd13ea08cefc5b4dedb475ce11f91579ab9555
Cory Gorham <Borham@Administrators-MacBook-Pro-4.local>
2014-09-14 22:43:48 -0400

    working out main menu choice text

--- src/calculatorapp/CalculatorApp.java
+++ src/calculatorapp/CalculatorApp.java
@@ -1,11 +1,13 @@
 package calculatorapp;
-
+import java.lang.math;
 /**
  * Created by Borham on 9/14/14.
  */
 public class CalculatorApp {
     double num1;
     double num2;
+
+    //creating a lot of useless methods here... just trying to get in the habit of making methods for things
 
     double addition(double add1, double add2){
         return (add1 + add2);
@@ -23,8 +25,30 @@ public class CalculatorApp {
         return (numerator / denominator);
     }
 
+    //ok this one is really useless.... but yeah... same thing as before
 
+    double squareRoot(double number){
+        return (Math.sqrt(number));
+    }
+
+
+    
     public static void main(String[] args) {
+
+        int choice;
+        boolean valid;
+
+
+
+        System.out.println("==SUPER CALCULATOR==");
+        System.out.println();
+        System.out.println("  1) Addition");
+        System.out.println("  2) Subtraction)");
+        System.out.println("  3) Multiplication");
+        System.out.println("  4) Division");
+        System.out.println("  5) Square Root");
+        System.out.println();
+        System.out.print("Please enter a number (1-5) to make your choice: ");
 
 
     }
