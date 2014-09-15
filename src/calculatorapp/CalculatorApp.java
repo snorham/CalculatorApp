a4899c2f2041c5cdb40e3abacf6634bf82a36d4d
Cory Gorham <Borham@Administrators-MacBook-Pro-4.local>
2014-09-14 21:56:57 -0400

    creating addition and subtraction methods in the java class

--- src/calculatorapp/CalculatorApp.java
+++ src/calculatorapp/CalculatorApp.java
@@ -4,4 +4,26 @@ package calculatorapp;
  * Created by Borham on 9/14/14.
  */
 public class CalculatorApp {
+    double num1;
+    double num2;
+
+    double addition(double add1, double add2){
+        return (add1 + add2);
+    }
+
+    double subtraction(double sub1, double sub2){
+        return (sub1 - sub2);
+    }
+
+    
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
 }
