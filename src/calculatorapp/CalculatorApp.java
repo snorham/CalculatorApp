c511dd14015503a0aa5f18399186f7b8f4516bb8
Cory Gorham <Borham@Administrators-MacBook-Pro-4.local>
2014-09-14 23:58:27 -0400

    adding switch statement to handle menu choices

--- src/calculatorapp/CalculatorApp.java
+++ src/calculatorapp/CalculatorApp.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
  * Created by Borham on 9/14/14.
  */
 public class CalculatorApp {
-    double num1;
-    double num2;
+    //creating a lot of methods here, most of them are useless... just trying to get in the habit of making methods for things
 
-    //creating a lot of useless methods here... just trying to get in the habit of making methods for things
-double doubleChecker(){
+public static double doubleChecker(){
     Scanner doubleScan = new Scanner(System.in);
     do {
         if (doubleScan.hasNextDouble()) {
+            System.out.println();
             return (doubleScan.nextDouble());
         }
         else {
@@ -25,19 +24,19 @@ double doubleChecker(){
     }while(true);
 }
 
-    double addition(double add1, double add2){
+    public static double addition(double add1, double add2){
         return (add1 + add2);
     }
 
-    double subtraction(double sub1, double sub2){
+    public static double subtraction(double sub1, double sub2){
         return (sub1 - sub2);
     }
 
-    double multiplication(double multiply1, double multiply2){
+    public static double multiplication(double multiply1, double multiply2){
         return (multiply1 * multiply2);
     }
 
-    double division(double numerator, double denominator){
+    public static double division(double numerator, double denominator){
         return (numerator / denominator);
     }
 
@@ -48,7 +47,7 @@ double doubleChecker(){
     }
 
     public static int calcMenu(){
-        int choice;
+        int choice = 0;
         boolean valid;
         Scanner scanIt = new Scanner(System.in);
 
@@ -66,10 +65,12 @@ double doubleChecker(){
             if (scanIt.hasNextInt()) {
                 choice = scanIt.nextInt();
                 if ((choice > 0) && (choice < 6)) {
+                    System.out.println();
                     valid = true;
                 }
 
                 else {
+                    System.out.println();
                     System.out.println("[Error: Incorrect Entry]");
                     System.out.print("Please enter a number (1-5) to make your choice: ");
                     valid = false;
@@ -89,8 +90,16 @@ double doubleChecker(){
     }
 
     public static void main(String[] args) {
+        double num1, num2;
+
         switch (calcMenu()){
-            case 1:  System.out.println("Please enter a value to add:");
+            case 1:
+                System.out.print("Please enter a numeric value to add: ");
+                num1 = doubleChecker();
+                System.out.print("Please enter a second numeric value to add: ");
+                num2 = doubleChecker();
+                System.out.println("The sum of " + num1 + " and " + num2 + " is " + addition(num1,num2));
+                break;
             case 2:
             case 3:
             case 4:
